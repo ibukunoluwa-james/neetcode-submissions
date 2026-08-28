d5f1f706dcf77d630ec3708941019dadef79f9dc
James Ibukunoluwa <101001264+ibukunoluwa-james@users.noreply.github.com>
2026-08-28 23:18:31 +0100

    Add: two-integer-sum-ii - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-0.ts
@@ -0,0 +1,24 @@
+class Solution {
+    /**
+     * @param {number[]} numbers
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(numbers: number[], target: number): number[] {
+        let i = 0;
+        let j = numbers.length - 1;
+
+        while (true) {
+            if (numbers[i] + numbers[j] == target) {
+                return [i + 1, j + 1];
+            }
+            else if (numbers[i] + numbers[j] > target) {
+                j--;
+            }
+            else {
+                i++
+            }
+        }
+
+    }
+}
