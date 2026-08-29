238ba18d4b706b038c9a20837c387c8b3d09981f
James Ibukunoluwa <101001264+ibukunoluwa-james@users.noreply.github.com>
2026-08-29 08:11:00 +0100

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.ts
@@ -0,0 +1,33 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums: number[]): number[][] {
+        nums.sort((a, b) => a - b);
+
+        let result = [];
+
+        for (let k = 0; k < nums.length; k++) {
+            if (nums[k] !== nums[k - 1]) {
+                let target = -nums[k];
+                let i = k + 1;
+                let j = nums.length - 1;
+                while (i < j) {
+                    // loop runs infinitely without breaking
+                    if (nums[i] + nums[j] == target) {
+                        result.push([nums[k], nums[i], nums[j]]);
+                        i++;
+                        j--;
+                        while (nums[i] === nums[i - 1]) i++;
+                    } else if (nums[i] + nums[j] > target) {
+                        j--;
+                    } else {
+                        i++;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+}
