948fc9d901321fcab4a3898eaee44d218d44e9b6
James Ibukunoluwa <101001264+ibukunoluwa-james@users.noreply.github.com>
2026-09-04 15:40:37 +0100

    Add: longest-substring-without-duplicates - submission-5

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-5.ts
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {string} s
+     * @return {number}
+     */
+    lengthOfLongestSubstring(s: string): number {
+        let seen = new Set<string>(),
+        left = 0, max = 0;
+
+        for (let right = 0; right < s.length; right++) {
+            while (seen.has(s[right])) {
+                seen.delete(s[left]);
+                left++
+            }
+            seen.add(s[right])
+            max = Math.max(max, seen.size)
+        }
+
+        return max
+    }
+}
