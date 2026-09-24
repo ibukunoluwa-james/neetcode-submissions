5318eac9cc44864926efe8690423876e29398fe8
James Ibukunoluwa <101001264+ibukunoluwa-james@users.noreply.github.com>
2026-09-24 15:02:31 +0100

    Add: find-k-closest-elements - submission-5

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-5.ts
@@ -0,0 +1,27 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @param {number} k
+     * @param {number} x
+     * @return {number[]}
+     */
+    findClosestElements(arr: number[], k: number, x: number): number[] {
+        let left = 0;
+        let right = arr.length - k;
+
+
+        
+        
+        // for (let i = 0; i < arr.length; i++) {
+            while (left < right) {
+                const mid = Math.floor((left + right) / 2)
+                if (x - arr[mid] > arr[mid + k] - x) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+        // }
+        return arr.slice(left, left + k);
+    }
+}
